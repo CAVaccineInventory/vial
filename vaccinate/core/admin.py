91039de5283b05e62db7d6314cd2d4065a21b385
Simon Willison <swillison@gmail.com>
2021-02-24 08:31:48 -0800

    Better admin configuration, refs #13

--- vaccinate/core/admin.py
+++ vaccinate/core/admin.py
@@ -1,9 +1,127 @@
-from django.apps import apps
 from django.contrib import admin
+from .models import (
+    LocationType,
+    ProviderType,
+    Provider,
+    State,
+    County,
+    Location,
+    Reporter,
+    AvailabilityTag,
+    AppointmentTag,
+    CallReport,
+    EvaReport,
+    CallRequestReason,
+    CallRequest,
+    PublishedReport,
+)
+
+# Simple models first
+for model in (LocationType, ProviderType, State):
+    admin.site.register(model)
 
 
-models = apps.get_models()
+@admin.register(Provider)
+class ProviderAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "main_url", "provider_type")
+    list_filter = ("provider_type",)
 
-for model in models:
-    if model.__module__ == "core.models":
-        admin.site.register(model)
+
+@admin.register(County)
+class CountyAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "state", "fips_code")
+    list_filter = ("state",)
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    search_fields = ("name", "full_address")
+    list_display = (
+        "name",
+        "full_address",
+        "state",
+        "county",
+        "location_type",
+        "provider",
+        "soft_deleted",
+    )
+    list_filter = ("location_type", "state", "provider", "soft_deleted")
+    raw_id_fields = ("county", "provider", "duplicate_of")
+
+
+@admin.register(Reporter)
+class ReporterAdmin(admin.ModelAdmin):
+    search_fields = ("airtable_name", "auth0_name")
+    list_display = ("airtable_name", "auth0_name", "auth0_role_name")
+    list_filter = ("auth0_role_name",)
+
+
+@admin.register(AvailabilityTag)
+class AvailabilityTagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "notes", "disabled")
+    list_filter = ("disabled",)
+
+
+@admin.register(AppointmentTag)
+class AppointmentTagAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_display = ("name", "has_details")
+
+
+@admin.register(CallReport)
+class CallReportAdmin(admin.ModelAdmin):
+    list_display = ("location", "report_source", "reported_by", "created_at")
+    raw_id_fields = ("location", "reported_by", "call_request", "availability_tags")
+    list_filter = ("created_at", "report_source")
+
+
+@admin.register(EvaReport)
+class EvaReportAdmin(admin.ModelAdmin):
+    list_display = (
+        "location",
+        "name_from_import",
+        "has_vaccines",
+        "hung_up",
+        "valid_at",
+    )
+    raw_id_fields = ("location",)
+    list_filter = ("valid_at", "has_vaccines")
+
+
+@admin.register(CallRequestReason)
+class CallRequestReasonAdmin(admin.ModelAdmin):
+    list_display = ("short_reason", "long_reason")
+
+
+@admin.register(CallRequest)
+class CallRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "location",
+        "vesting_at",
+        "claimed_by",
+        "claimed_until",
+        "call_request_reason",
+    )
+    list_filter = ("call_request_reason",)
+    raw_id_fields = ("claimed_by", "tip_report")
+
+
+@admin.register(PublishedReport)
+class PublishedReportAdmin(admin.ModelAdmin):
+    list_display = (
+        "location",
+        "appointment_tag",
+        "reported_by",
+        "valid_at",
+        "created_at",
+    )
+    raw_id_fields = (
+        "location",
+        "reported_by",
+        "availability_tags",
+        "call_reports",
+        "eva_reports",
+    )
