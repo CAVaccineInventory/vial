a2672648b4c41ae98d59be04ef9f7a95e9c87d6d
Simon Willison <swillison@gmail.com>
2021-02-23 21:57:15 -0800

    Test to enforce .SecurityMiddleware is first, refs #8

--- vaccinate/core/tests.py
+++ vaccinate/core/tests.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from .models import State
 import pytest
 
@@ -5,3 +6,7 @@ import pytest
 @pytest.mark.django_db
 def test_states_were_populated():
     assert State.objects.count() == 50
+
+
+def test_security_middleware_is_first():
+    assert settings.MIDDLEWARE[0] == "django.middleware.security.SecurityMiddleware"
