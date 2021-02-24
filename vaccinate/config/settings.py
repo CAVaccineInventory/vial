913f7cf1ac3687fdf4d9637a3518f3eaf981f7ad
Simon Willison <swillison@gmail.com>
2021-02-23 19:58:59 -0800

    Added django_heroku, refs #6

--- vaccinate/config/settings.py
+++ vaccinate/config/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 import dj_database_url
 from pathlib import Path
 import os
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
