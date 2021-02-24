001ed3b48b0e8d098ce0fdf90a85437944e299d7
Simon Willison <swillison@gmail.com>
2021-02-23 20:08:27 -0800

    Removed more django_heroku, refs #6

--- vaccinate/config/settings.py
+++ vaccinate/config/settings.py
@@ -1,4 +1,3 @@
-import django_heroku
 import dj_database_url
 from pathlib import Path
 import os
@@ -124,11 +123,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = "/static/"
-
-django_heroku.settings(locals())
