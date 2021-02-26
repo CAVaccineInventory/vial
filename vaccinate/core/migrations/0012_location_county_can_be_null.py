d28b5edce973c7e6564a6f0ad81c5f00992f7382
Simon Willison <swillison@gmail.com>
2021-02-25 23:34:53 -0800

    Applied Black, refs #15

--- vaccinate/core/migrations/0012_location_county_can_be_null.py
+++ vaccinate/core/migrations/0012_location_county_can_be_null.py
@@ -7,13 +7,19 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0011_more_availability_tags'),
+        ("core", "0011_more_availability_tags"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='location',
-            name='county',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, related_name='locations', to='core.county'),
+            model_name="location",
+            name="county",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="locations",
+                to="core.county",
+            ),
         ),
     ]
