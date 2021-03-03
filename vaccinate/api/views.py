4b77c24a79aa516498d0a7d27b09b117a3a74787
Simon Willison <swillison@gmail.com>
2021-03-03 09:37:08 -0800

    ?test=1&fake_user=x API debug option, refs #29

--- vaccinate/api/views.py
+++ vaccinate/api/views.py
@@ -25,28 +25,49 @@ class ReportValidator(BaseModel):
             return Location.objects.get(public_id=v)
         except Location.DoesNotExist:
             raise ValueError("Location '{}' does not exist".format(v))
-        return v
 
 
 @csrf_exempt
 def submit_report(request):
-    authorization = request.META.get("HTTP_AUTHORIZATION") or ""
-    if not authorization.startswith("Bearer "):
-        return JsonResponse({"error": "Authorization header must start with 'Bearer'"})
-    # Check JWT token is valid
-    jwt_id_token = authorization.split("Bearer ")[1]
-    try:
-        jwt_payload = decode_and_verify_jwt(jwt_id_token)
-    except Exception as e:
-        return JsonResponse({"error": "Could not decode JWT", "details": str(e)})
+    # The ?test=1 version accepts &fake_user=external_id
+    reporter = None
+    user_info = {}
+    if bool(request.GET.get("test")) and request.GET.get("fake_user"):
+        reporter = Reporter.objects.get_or_create(
+            external_id="auth0:{}".format(request.GET["fake_user"]),
+        )[0]
+        user_info = {"fake": reporter.external_id}
+    else:
+        authorization = request.META.get("HTTP_AUTHORIZATION") or ""
+        if not authorization.startswith("Bearer "):
+            return JsonResponse(
+                {"error": "Authorization header must start with 'Bearer'"}
+            )
+        # Check JWT token is valid
+        jwt_id_token = authorization.split("Bearer ")[1]
+        try:
+            jwt_payload = decode_and_verify_jwt(jwt_id_token)
+        except Exception as e:
+            return JsonResponse({"error": "Could not decode JWT", "details": str(e)})
+        reporter = Reporter.objects.update_or_create(
+            external_id="auth0:{}".format(jwt_payload["sub"]),
+            defaults={
+                "name": jwt_payload["name"],
+                "auth0_role_name": ", ".join(
+                    sorted(jwt_payload.get("https://help.vaccinateca.com/roles", []))
+                ),
+            },
+        )[0]
+        user_info = jwt_payload
+
     try:
         post_data = json.loads(request.body.decode("utf-8"))
     except ValueError as e:
-        return JsonResponse({"error": str(e), "user": jwt_payload})
+        return JsonResponse({"error": str(e), "user": user_info})
     try:
         report_data = ReportValidator(**post_data).dict()
     except ValidationError as e:
-        return JsonResponse({"error": e.errors(), "user": jwt_payload})
+        return JsonResponse({"error": e.errors(), "user": user_info})
     # Now we add the report
     appointment_tag_string, appointment_details = derive_appointment_tag(
         report_data["appointments_by_phone"], report_data["appointment_details"]
@@ -61,15 +82,7 @@ def submit_report(request):
         appointment_details=appointment_details,
         public_notes=report_data["public_notes"],
         internal_notes=report_data["internal_notes"],
-        reported_by=Reporter.objects.update_or_create(
-            external_id="auth0:{}".format(jwt_payload["sub"]),
-            defaults={
-                "name": jwt_payload["name"],
-                "auth0_role_name": ", ".join(
-                    sorted(jwt_payload.get("https://help.vaccinateca.com/roles", []))
-                ),
-            },
-        )[0],
+        reported_by=reporter,
     )
     for tag_model in availability_tags:
         report.availability_tags.add(tag_model)
@@ -85,7 +98,7 @@ def submit_report(request):
             "appointment_details": appointment_details,
             "availability_tags": [str(a) for a in availability_tags],
             "report": str(report),
-            "user": jwt_payload,
+            "user_info": user_info,
         }
     )
 
