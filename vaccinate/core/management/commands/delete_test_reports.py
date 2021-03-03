1896fe0836460932f65e949d557e6d1a3dd28be8
Simon Willison <swillison@gmail.com>
2021-03-03 10:58:37 -0800

    ./manage.py delete_test_reports, refs #29

---
+++ vaccinate/core/management/commands/delete_test_reports.py
@@ -0,0 +1,9 @@
+from core.models import Report
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    "Delete all test data from the Report table"
+
+    def handle(self, *args, **options):
+        print("Deleted: {}".format(Report.objects.filter(is_test_data=True).delete()))
