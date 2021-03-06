3d2e2321c63781761078f48900892dad89a94b50
Alex Vandiver <alex@chmrr.net>
2021-03-06 05:41:34 -0800

    migrations: Enforce that they must be backwards compatible.
    Since migrations may happen while traffic is still being served by the
    previous version, it is important that they be backwards-compatible;
    that is, no column or table drops or renames unless production has
    already stopped relying on them.
    
    Add a linter which validates this property.  Since there have
    _already_ been non-backwards-compatible changes (which were fine, it's
    only going forward that matters!), we pass it a "high water mark" of
    a recent commit on main; only migrations after that point are checked.
    
    Because this relies on commit history in CI, it requires a non-shallow
    clone in the github checkout action.  Particularly for small
    repositories, shallow clones are significantly more CPU intensive, for
    only tiny gain; switching to a deep clone is not a notable cost.

--- vaccinate/config/settings.py
+++ vaccinate/config/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_migration_linter",
     "social_django",
     "auth0login",
     "core",
