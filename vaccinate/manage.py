b627430ca644d77a116d21dfcf66b1d43c3a1006
Alex Vandiver <alex@chmrr.net>
2021-03-05 23:09:18 -0800

    manage: Use ipython for shell.
    This allows us to pre-load common models, and get tab completion from
    them.

--- vaccinate/manage.py
+++ vaccinate/manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("PYTHONSTARTUP", os.path.join(BASE_DIR, "config/pythonrc.py"))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
