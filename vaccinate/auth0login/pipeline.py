da81ffc3b2c570063fdec89065a54cacda318c19
Simon Willison <swillison@gmail.com>
2021-03-08 14:50:37 -0800

    'VIAL super-user' role grants user.is_superuser, refs #89

--- vaccinate/auth0login/pipeline.py
+++ vaccinate/auth0login/pipeline.py
@@ -5,9 +5,16 @@ def provide_admin_access_based_on_auth0_role(backend, user, response, *args, **k
     if backend.name == "auth0":
         roles = kwargs.get("details", {}).get("roles", {}) or []
         should_be_staff = "Vaccinate CA Staff" in roles
+        should_be_superuser = "VIAL super-user" in roles
         group = Group.objects.get_or_create(name="default-view-core")[0]
+        needs_save = False
         if should_be_staff != user.is_staff:
             user.is_staff = should_be_staff
+            needs_save = True
+        if should_be_superuser != user.is_superuser:
+            user.is_superuser = should_be_superuser
+            needs_save = True
+        if needs_save:
             user.save()
         # Ensure user has membership of group (or not)
         if should_be_staff:
