b50e18085e507b961d4b0a6eeb6aaa3e93db24a2
Simon Willison <swillison@gmail.com>
2021-03-09 17:22:25 -0800

    ApiKey model, refs #100

--- vaccinate/api/models.py
+++ vaccinate/api/models.py
@@ -1,6 +1,28 @@
+import secrets
+
 from core.fields import CharTextField
 from django.db import models
 from django.utils import timezone
+
+
+def random_secret():
+    return secrets.token_hex(16)
+
+
+class ApiKey(models.Model):
+    created_at = models.DateTimeField(
+        default=timezone.now, help_text="When the API key was created"
+    )
+    last_seen_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text="Last time API key was seen, accurate to within one minute",
+    )
+    description = models.TextField(help_text="What this API key is being used for")
+    key = models.CharField(max_length=32, default=random_secret)
+
+    def __str__(self):
+        return "{}:{}...".format(self.pk, self.key[:8])
 
 
 class ApiLog(models.Model):
@@ -36,6 +58,9 @@ class ApiLog(models.Model):
         related_name="created_by_api_logs",
         on_delete=models.SET_NULL,
         help_text="Report that was created by this API call, if any",
+    )
+    api_key = models.ForeignKey(
+        ApiKey, on_delete=models.SET_NULL, null=True, blank=True
     )
 
     class Meta:
