bdfb18685b88155b65e0147d1da5efaed3d98dc3
Simon Willison <swillison@gmail.com>
2021-03-10 08:15:13 -0800

    Added /admin/commands/ import_counties tool, refs #88

--- vaccinate/core/views.py
+++ vaccinate/core/views.py
@@ -1,6 +1,9 @@
 import os
 import sys
+from io import StringIO
 
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core import management
 from django.db import connection
 from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
@@ -24,4 +27,27 @@ def healthcheck(request):
             "postgresql_version": cursor.fetchone()[0],
             "python_version": sys.version,
         }
+    )
+
+
+@login_required
+@user_passes_test(lambda user: user.is_superuser)
+def admin_commands(request):
+    command_output = StringIO()
+    error = None
+    if request.method == "POST":
+        command_to_run, args = {
+            "import_counties": ("import_counties", []),
+        }.get(request.POST.get("command"), (None, None))
+        if command_to_run is None:
+            error = "Unknown command"
+        else:
+            management.call_command(command_to_run, *args, stdout=command_output)
+    return render(
+        request,
+        "admin/commands.html",
+        {
+            "output": command_output.getvalue(),
+            "error": error,
+        },
     )
