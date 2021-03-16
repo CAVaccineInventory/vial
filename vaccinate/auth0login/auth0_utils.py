4608c664fc3a9b994797dc75edd3968622b1ba8d
Alex Vandiver <alex@vaccinateca.com>
2021-03-16 13:47:29 -0700

    api: Add more tracing spans around auth.
    Traces show that there are several hundred milliseconds of time before
    the first database calls; add additional spans for early actions (like
    auth) that might be at fault.

--- vaccinate/auth0login/auth0_utils.py
+++ vaccinate/auth0login/auth0_utils.py
@@ -1,5 +1,6 @@
 import functools
 
+import beeline
 import requests
 from django.conf import settings
 from jose import jwt
@@ -8,11 +9,13 @@ DOMAIN = settings.SOCIAL_AUTH_AUTH0_DOMAIN
 
 
 @functools.cache
+@beeline.traced(name="jwks")
 def jwks():
     jwt_keys_url = "https://" + DOMAIN + "/.well-known/jwks.json"
     return requests.get(jwt_keys_url, timeout=5).content
 
 
+@beeline.traced(name="decode_and_verify_jwt")
 def decode_and_verify_jwt(jwt_id_token, try_fallback=False):
     # Verifies the signature of a JWT and returns the decoded payload
     try:
@@ -23,14 +26,17 @@ def decode_and_verify_jwt(jwt_id_token, try_fallback=False):
             audience=settings.SOCIAL_AUTH_AUTH0_KEY,
             issuer="https://" + DOMAIN + "/",
         )
-    except Exception:
+    except Exception as e:
+        beeline.add_context({"exception": e})
         if try_fallback:
-            fallback_response = requests.get(
-                "https://vaccinateca.us.auth0.com/userinfo",
-                headers={"Authorization": "Bearer {}".format(jwt_id_token)},
-                timeout=5,
-            )
-            fallback_response.raise_for_status()
-            return fallback_response.json()
+            with beeline.tracer(name="auth0-fallback"):
+                fallback_response = requests.get(
+                    "https://vaccinateca.us.auth0.com/userinfo",
+                    headers={"Authorization": "Bearer {}".format(jwt_id_token)},
+                    timeout=5,
+                )
+                beeline.add_context({"status", fallback_response.status_code})
+                fallback_response.raise_for_status()
+                return fallback_response.json()
         else:
             raise
