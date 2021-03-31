303c0719c80d7ab493221f3e03ac9549ed904f65
Simon Willison <swillison@gmail.com>
2021-03-31 13:50:12 -0700

    Show tools menu if user has permissions, closes #154

--- vaccinate/config/menu.py
+++ vaccinate/config/menu.py
@@ -3,11 +3,31 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
+class ToolsMenu(items.MenuItem):
+    title = "Tools"
+
+    def init_with_context(self, context):
+        user = context["request"].user
+        self.children = []
+        if user.has_perm("django_sql_dashboard.execute_sql"):
+            self.children.append(
+                items.MenuItem("SQL Dashboard", "/dashboard/"),
+            )
+        if user.has_perm("core.merge_locations"):
+            self.children.append(
+                items.MenuItem("Merge locations", "/admin/merge-locations/"),
+            )
+
+    def is_empty(self):
+        return not bool(self.children)
+
+
 class CustomMenu(Menu):
     def __init__(self, **kwargs):
         Menu.__init__(self, **kwargs)
         self.children += [
             items.MenuItem(_("Home"), reverse("admin:index")),
+            ToolsMenu(),
             items.AppList(_("Applications"), exclude=("django.contrib.*",)),
             items.AppList(_("Administration"), models=("django.contrib.*",)),
             items.Bookmarks(),
