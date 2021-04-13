7e0e2f9dc43565d8894a0c06b18fb190ecdd47a4
Simon Willison <swillison@gmail.com>
2021-04-13 08:46:11 -0700

    Revert "Custom location CSV export with phone and website, closes #221"
    This reverts commit d16330c5c0c98e521a162ccfe9ff3644eabaa52c.
    
    This was meant to go on another branch

--- vaccinate/core/admin_actions.py
+++ vaccinate/core/admin_actions.py
@@ -25,8 +25,6 @@ def export_as_csv_action(
     customize_queryset=None,
     extra_columns=None,
     extra_columns_factory=None,
-    specific_columns=None,
-    suffix=None,
 ):
     extra_columns = extra_columns or []
     customize_queryset = customize_queryset or (lambda qs: qs)
@@ -37,15 +35,11 @@ def export_as_csv_action(
             csvfile = StringIO()
             csvwriter = csv.writer(csvfile)
             columns = []
-            if specific_columns:
-                # Column headers are the keys of this dict
-                columns = list(specific_columns.keys())
-            else:
-                for field in modeladmin.model._meta.fields:
-                    if isinstance(field, ForeignKey):
-                        columns.extend([field.attname, field.name])
-                    else:
-                        columns.append(field.name)
+            for field in modeladmin.model._meta.fields:
+                if isinstance(field, ForeignKey):
+                    columns.extend([field.attname, field.name])
+                else:
+                    columns.append(field.name)
             if extra_columns:
                 columns.extend(extra_columns)
 
@@ -64,31 +58,22 @@ def export_as_csv_action(
                 yield read_and_flush()
 
             for row in keyset_pagination_iterator(queryset):
-                if specific_columns:
-                    csv_row = [
-                        getattr(row, actual_column)
-                        for actual_column in specific_columns.values()
-                    ]
-                else:
-                    csv_row = [
-                        getattr(row, column)
-                        for column in columns
-                        if column not in extra_columns
-                    ]
+                csv_row = [
+                    getattr(row, column)
+                    for column in columns
+                    if column not in extra_columns
+                ]
                 if extra_columns_factory:
                     csv_row.extend(extra_columns_factory(row))
                 csvwriter.writerow(csv_row)
                 yield read_and_flush()
 
-        filename = modeladmin.model.__name__
-        if suffix:
-            filename += "_{}".format(suffix)
         response = StreamingHttpResponse(rows(queryset), content_type="text/csv")
-        response["Content-Disposition"] = "attachment; filename=%s.csv" % filename
+        response["Content-Disposition"] = (
+            "attachment; filename=%s.csv" % modeladmin.model.__name__
+        )
 
         return response
 
     export_as_csv.short_description = description
-    if suffix:
-        export_as_csv.__name__ = "export_as_csv_{}".format(suffix)
     return export_as_csv
