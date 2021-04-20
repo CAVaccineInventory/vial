78fa3503f80570f1ee5b9dbd955b2be4bcdec624
Simon Willison <swillison@gmail.com>
2021-04-19 18:02:05 -0700

    Fixed incorrect migration dependency

--- vaccinate/core/migrations/0093_sourcelocation_matched_location.py
+++ vaccinate/core/migrations/0093_sourcelocation_matched_location.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0092_sourcelocation_matched_location"),
+        ("core", "0092_report_call_request_set_null"),
     ]
 
     operations = [
