5360db053f9ad3c3280f061df51add72b20d80a0
Simon Willison <swillison@gmail.com>
2021-04-19 18:01:22 -0700

    Fixed duplicate 0092 migrations

--- vaccinate/core/migrations/0093_sourcelocation_matched_location.py
+++ vaccinate/core/migrations/0093_sourcelocation_matched_location.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0091_concordanceidentifier"),
+        ("core", "0092_sourcelocation_matched_location"),
     ]
 
     operations = [
