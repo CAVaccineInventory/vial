34a7c50a22dabc894edaa929c174ee23ec32f0f2
Simon Willison <swillison@gmail.com>
2021-04-20 14:50:03 -0700

    Migration to initially populate concordances, closes #370

---
+++ vaccinate/core/migrations/0094_populate_concordances_from_existing_ids.py
@@ -0,0 +1,38 @@
+from django.db import migrations
+
+# https://github.com/CAVaccineInventory/vial/issues/370
+
+concordances = (
+    # Location column, ConcordanceIdentifier source
+    ("google_places_id", "google_places"),
+    ("vaccinespotter_location_id", "vaccinespotter"),
+    ("vaccinefinder_location_id", "vaccinefinder"),
+)
+
+
+def populate_concordances(apps, schema_editor):
+    Location = apps.get_model("core", "Location")
+    ConcordanceIdentifier = apps.get_model("core", "ConcordanceIdentifier")
+
+    for column, source in concordances:
+        locations = Location.objects.exclude(**{f"{column}__isnull": True})
+        for location in locations:
+            identifier = getattr(location, column)
+            concordance_identifier = ConcordanceIdentifier.objects.get_or_create(
+                source=source,
+                identifier=identifier,
+            )[0]
+            location.concordances.add(concordance_identifier)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("core", "0093_sourcelocation_matched_location"),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            populate_concordances, reverse_code=lambda apps, schema_editor: None
+        ),
+    ]
