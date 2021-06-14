691ed62aca6d196186012931a21b596b78564764
Ugaso <ugotsoul@users.noreply.github.com>
2021-06-14 11:10:32 -0700

    Reflect auth0 roles in VIAL (#660)
    * Add/Sync auth0 roles and update staff role dict
    
    * update roles on reporter model
    
    * Changes:
    - filter groups by environment
    - update env specific groups on user model
    - add readability to auth0 roles on reporter model
    
    * fix: add/remove current groups to user
    
    * only add local roles to reporter model
    
    * fix: don't index reporter queryset if its empty
    
    * Get staff roles in different envs using a function

--- vaccinate/auth0login/pipeline.py
+++ vaccinate/auth0login/pipeline.py
@@ -1,46 +1,68 @@
+from core.models import Reporter
 from django.conf import settings
 from django.contrib.auth import logout
 from django.contrib.auth.models import Group
 
-AUTH0_ROLES_TO_REFLECT = {
-    "Vaccinate CA Staff",
-    "VIAL super-user",
+AUTH0_ROLES_STAFF = {
     "Reports QA",
+    "VIAL admin",
     "VIAL data corrections",
     "VIAL service account",
     "VIAL WB limited",
+    "VIAL super-user",
+    "Vaccinate CA Staff",
 }
 
-AUTH0_ROLES_TO_LOCAL_GROUP = {}
-if settings.STAGING:
-    AUTH0_ROLES_TO_LOCAL_GROUP = {
-        name + " STAGING": name for name in AUTH0_ROLES_TO_REFLECT
-    }
-else:
-    AUTH0_ROLES_TO_LOCAL_GROUP = {name: name for name in AUTH0_ROLES_TO_REFLECT}
+
+def get_staff_roles_by_env(staff):
+    if settings.STAGING:
+        return [name + " STAGING" for name in AUTH0_ROLES_STAFF]
+    else:
+        return [name for name in AUTH0_ROLES_STAFF]
+
+
+def get_roles_by_env(role):
+    if settings.STAGING:
+        return "STAGING" in role
+    else:
+        return "STAGING" not in role
 
 
 def provide_admin_access_based_on_auth0_role(backend, user, response, *args, **kwargs):
     if backend.name == "auth0":
         users_roles = kwargs.get("details", {}).get("roles", {}) or []
+        local_roles = list(filter(get_roles_by_env, users_roles))
         groups = {
-            name: Group.objects.get_or_create(name=name)[0]
-            for name in AUTH0_ROLES_TO_LOCAL_GROUP.values()
+            name: Group.objects.get_or_create(name=name)[0] for name in local_roles
         }
+        staff_roles_to_local_group = get_staff_roles_by_env(AUTH0_ROLES_STAFF)
         should_be_staff = any(
-            auth0_role in users_roles
-            for auth0_role in AUTH0_ROLES_TO_LOCAL_GROUP.keys()
+            auth0_role in users_roles for auth0_role in staff_roles_to_local_group
         )
+
         if should_be_staff != user.is_staff:
             user.is_staff = should_be_staff
             user.save()
-        # Add user to groups if necessary:
-        for auth0_role, local_group in AUTH0_ROLES_TO_LOCAL_GROUP.items():
-            group = groups[local_group]
-            if auth0_role in users_roles:
+
+        # Update user's group membership
+        # TODO: Update groups after logging in using a webhook - see Issue #663
+        user.groups.clear()
+
+        for name, group in groups.items():
+            if name in local_roles:
                 group.user_set.add(user)
             else:
                 group.user_set.remove(user)
+
+        # Note: Not all users are reporters
+        reporter_qs = Reporter.objects.filter(user=user)
+
+        # Update auth0 roles on Reporter
+        if reporter_qs:
+            reporter = reporter_qs[0]
+            reporter.auth0_role_names = ", ".join(local_roles)
+            reporter.save()
+
         # Stash the id_token as 'jwt' in the session
         kwargs["request"].session["jwt"] = response["id_token"]
 
